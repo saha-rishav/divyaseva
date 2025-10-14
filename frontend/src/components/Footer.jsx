d3e734c32ad3260935bc65fffcb1f65061cf7494
saha-rishav <rishavsaha199@gmail.com>
2025-10-14 13:15:07 +0530

    footer section done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-center py-6 bg-[#9f2500] text-yellow-200">
+      <p>© {new Date().getFullYear()} DivyaSeva | All Rights Reserved</p>
+      {/* <p className="text-yellow-300 mt-2">Contact: support@divyaseva.in</p> */}
+    </footer>
   )
 }
 
