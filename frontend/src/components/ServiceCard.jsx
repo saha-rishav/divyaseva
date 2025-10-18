13d7ff3755e55f226d060f5fd1161aba5715b80e
saha-rishav <rishavsaha199@gmail.com>
2025-10-18 21:02:37 +0530

    service card hover image scale done

--- frontend/src/components/ServiceCard.jsx
+++ frontend/src/components/ServiceCard.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const ServiceCard = ({ title, description, image }) => {
     return (
         <Link to="/service-details"
-            className="group w-full text-center bg-card p-4 md:p-5 rounded-xl cursor-pointer transition-transform duration-400 hover:-translate-y-1.5 border-[2px] border-transparent shadow-xl">
+            className="group w-full text-center bg-card p-4 md:p-5 rounded-xl cursor-pointer transition-transform duration-400 hover:-translate-y-1.5 border-[3px] hover:border-primary border-transparent shadow-xl">
 
             <div className="overflow-hidden rounded-xl">
                 <img
                     src={image}
                     alt={title}
-                    className="w-full md:w-[400px] h-36 rounded-xl shadow-lg transform transition-transform duration-500 ease-in-out group-hover:scale-105 overflow-hidden object-contain"
+                    className="w-full h-36 rounded-xl shadow-lg transform transition-transform duration-500 ease-in-out group-hover:scale-105 overflow-hidden object-contain"
                 />
             </div>
 
--- frontend/src/components/ServiceDetails.jsx
+++ frontend/src/components/ServiceDetails.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ServiceDetails = () => {
   return (
-    <div>ServiceDetails</div>
+    <div className="w-[90%] max-w-7xl mx-auto pt-[30px]">
+        <h4>Back to Pujas</h4>
+    </div>
   )
 }
 
